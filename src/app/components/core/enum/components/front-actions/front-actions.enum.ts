7fe263f54d354ab2cd04d3b7e1bd3d0153814427
Lucas Ribeiro <lucasnunesr@live.com>
2023-01-24 04:24:04 -0300

    :hammer: added first version of mobile

--- src/app/components/core/enum/components/front-actions/front-actions.enum.ts
+++ src/app/components/core/enum/components/front-actions/front-actions.enum.ts
@@ -1,4 +1,5 @@
 export enum CARD_FRONT_ACTION_POSITION {
   LEFT = "position__left",
   RIGHT = "position__right",
+  NONE = "",
 }
--- src/app/components/shared/front-actions/card-action/card-action.component.ts
+++ src/app/components/shared/front-actions/card-action/card-action.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, HostListener, Input, OnInit } from "@angular/core";
+import { CARD_FRONT_ACTION_POSITION } from "src/app/components/core/enum/components/front-actions/front-actions.enum";
 import { CARD_FRONT_ACTION } from "src/app/components/core/interfaces/components/front-actions/front-actions.interface";
 
 @Component({
@@ -6,8 +7,27 @@ import { CARD_FRONT_ACTION } from "src/app/components/core/interfaces/components
   templateUrl: "./card-action.component.html",
   styleUrls: ["./card-action.component.scss"],
 })
-export class CardActionComponent {
+export class CardActionComponent implements OnInit {
 
   @Input() card!: CARD_FRONT_ACTION;
+  private _position!: CARD_FRONT_ACTION_POSITION;
+
+  @HostListener('window:resize', ['$event'])
+  public isMobile() {
+    this.checkOuterWidth();
+  }
+
+  public ngOnInit(): void {
+    this.checkOuterWidth();
+  }
+
+  private checkOuterWidth() {
+    if(window.outerWidth <= 500){
+      if(this.card.position !== CARD_FRONT_ACTION_POSITION.NONE) this._position = this.card.position;
+      this.card.position = CARD_FRONT_ACTION_POSITION.NONE;
+    } else {
+      this.card.position = this._position;
+    }
+  }
 
 }
